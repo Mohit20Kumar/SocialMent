35e93b68ca579422b4dcfbb3aa718b12a5ecbd96
mohit20kumar <mohitkumar20021220@gmail.com>
2023-12-19 23:00:38 +0530

    done without the post

---
+++ src/app/LensProvider.tsx
@@ -0,0 +1,17 @@
+'use client'
+import { LensConfig, production, development } from '@lens-protocol/react-web'
+import { bindings as wagmiBindings } from '@lens-protocol/wagmi'
+import { LensProvider as Provider } from '@lens-protocol/react-web'
+
+const lensConfig: LensConfig = {
+    bindings: wagmiBindings(),
+    environment: development,
+}
+
+export function LensProvider({ children }) {
+    return (
+        <Provider config={lensConfig}>
+            {children}
+        </Provider>
+    )
+}
